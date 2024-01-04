16fafb8e4d852cc7db3aa16b7bc6c8ab50e7aaa7
thsrite <thsrite@gmail.com>
2024-01-04 11:54:16 +0800

    fix 修复重命名

--- plugins/shortplaymonitor/__init__.py
+++ plugins/shortplaymonitor/__init__.py
@@ -48,7 +48,7 @@ class ShortPlayMonitor(_PluginBase):
     # 插件图标
     plugin_icon = "Amule_B.png"
     # 插件版本
-    plugin_version = "2.0"
+    plugin_version = "2.1"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
