61c5368f6ec3b614c264af564803f041ad5e8319
thsrite <thsrite@gmail.com>
2024-04-02 13:01:02 +0800

    fix 1.1

--- plugins/customcommand/__init__.py
+++ plugins/customcommand/__init__.py
@@ -22,7 +22,7 @@ class CustomCommand(_PluginBase):
     # 插件图标
     plugin_icon = "Ntfy_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
@@ -118,7 +118,7 @@ class CustomCommand(_PluginBase):
             if error:
                 logger.info(error.strip())
                 last_error = error.strip()
-
+        while True:
             output = result.stdout.readline().decode("utf-8")
             if output == '' and result.poll() is not None:
                 break
