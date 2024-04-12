90b3c14633f340c728d2d7d05ab71ccebbc9b895
thsrite <thsrite@gmail.com>
2024-04-12 16:54:10 +0800

    fix plugin order

--- plugins/pluginautoupdate/__init__.py
+++ plugins/pluginautoupdate/__init__.py
@@ -31,7 +31,7 @@ class PluginAutoUpdate(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "pluginautoupdate_"
     # 加载顺序
-    plugin_order = 99
+    plugin_order = 97
     # 可使用的用户级别
     auth_level = 1
 
