b0b76f51ab36dc78ea9a6a43cc8a35476b73d55e
thsrite <thsrite@gmail.com>
2024-04-26 15:11:22 +0800

    fix plugin_order

--- plugins/metarefresh/__init__.py
+++ plugins/metarefresh/__init__.py
@@ -30,7 +30,7 @@ class MetaRefresh(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "metarefresh_"
     # 加载顺序
-    plugin_order = 32
+    plugin_order = 15
     # 可使用的用户级别
     auth_level = 1
 
