b11b93d716c810d20142b482b09cc370cb30d642
thsrite <thsrite@gmail.com>
2024-05-06 21:21:59 +0800

    fix 增加历史删除按钮

--- plugins/actorsubscribe/__init__.py
+++ plugins/actorsubscribe/__init__.py
@@ -27,7 +27,7 @@ class ActorSubscribe(_PluginBase):
     # 插件图标
     plugin_icon = "Mdcng_A.png"
     # 插件版本
-    plugin_version = "1.5"
+    plugin_version = "1.6"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
