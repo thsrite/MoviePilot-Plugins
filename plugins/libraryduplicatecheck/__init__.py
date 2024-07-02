87654bf1e57b88643cbdaa42c6fd58ba8994677d
thsrite <thsrite@gmail.com>
2024-07-02 13:43:59 +0800

    fix order

--- plugins/libraryduplicatecheck/__init__.py
+++ plugins/libraryduplicatecheck/__init__.py
@@ -38,7 +38,7 @@ class LibraryDuplicateCheck(_PluginBase):
     # 插件配置项ID前缀
     plugin_config_prefix = "libraryduplicatecheck_"
     # 加载顺序
-    plugin_order = 9
+    plugin_order = 15
     # 可使用的用户级别
     auth_level = 2
 
