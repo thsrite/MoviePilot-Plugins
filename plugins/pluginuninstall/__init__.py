f9f7b85d3f00bfee9713594aebbc1fde4a6f3993
thsrite <thsrite@gmail.com>
2024-10-24 13:37:46 +0800

    fix 卸载时先停止注册服务

--- plugins/pluginuninstall/__init__.py
+++ plugins/pluginuninstall/__init__.py
@@ -21,7 +21,7 @@ class PluginUnInstall(_PluginBase):
     # 插件图标
     plugin_icon = "https://raw.githubusercontent.com/thsrite/MoviePilot-Plugins/main/icons/uninstall.png"
     # 插件版本
-    plugin_version = "1.1"
+    plugin_version = "1.2"
     # 插件作者
     plugin_author = "thsrite"
     # 作者主页
